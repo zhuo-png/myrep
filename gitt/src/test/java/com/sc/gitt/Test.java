ef6e539294247112e8488f1a9ea8543b40aee7cb
myc <myc@qq.com>
2021-02-23 16:38:34 +0800

    123

--- gitt/src/test/java/com/sc/gitt/Test.java
+++ gitt/src/test/java/com/sc/gitt/Test.java
@@ -3,7 +3,7 @@ package com.sc.gitt;
 public class Test {
     public static void main(String[] args) {
         int[] nums={1,2,3,4,6};
-
+        int c=333;
         for (int i = 0; i <nums.length-1 ; i++) {
 
         }
