6b0aab33743e3f363c30ab3b41c39103b331ad45
git <76165599699@qq.com>
2021-02-23 15:20:20 +0800

    第3次提交

--- gitt/src/test/java/com/sc/gitt/Test.java
+++ gitt/src/test/java/com/sc/gitt/Test.java
@@ -2,6 +2,11 @@ package com.sc.gitt;
 
 public class Test {
     public static void main(String[] args) {
+        int[] nums={1,2,3,4,6};
+
+        for (int i = 0; i <nums.length-1 ; i++) {
+
+        }
 
     }
 }
