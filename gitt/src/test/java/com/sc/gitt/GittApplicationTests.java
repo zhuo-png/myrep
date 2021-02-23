93416304ff217676e76715bbed587f5a28a072e1
git <76165599699@qq.com>
2021-02-23 14:08:33 +0800

    第一次提交

---
+++ gitt/src/test/java/com/sc/gitt/GittApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.gitt;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittApplicationTests {
+
+
+    void contextLoads() {
+    }
+
+}
---
+++ gitt/src/test/java/com/sc/gitt/Test.java
@@ -0,0 +1,4 @@
+package com.sc.gitt;
+
+public class Test {
+}
