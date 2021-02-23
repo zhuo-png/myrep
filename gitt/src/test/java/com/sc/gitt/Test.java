04b39adcd327d04565b02e9a202bfe181b7dd7b4
git <76165599699@qq.com>
2021-02-23 16:41:39 +0800

    第5次提交

--- gitt/src/test/java/com/sc/gitt/Test.java
+++ gitt/src/test/java/com/sc/gitt/Test.java
@@ -4,6 +4,9 @@ public class Test {
     public static void main(String[] args) {
         int[] nums={1,2,3,4,6};
         int c=333;
+        int x=60;
+        int z=20;
+
         for (int i = 0; i <nums.length-1 ; i++) {
 
         }
